1038f129459d3d3d7312b10d4423328fba159a99
vladimir sostaric <vladimirsostaric3390@gmail.com>
2023-09-05 11:05:38 +0200

    Add GroupAnagramNoSort

--- src/arrays/GroupAnagramsNoSort.java
+++ src/arrays/GroupAnagramsNoSort.java
@@ -1,6 +1,7 @@
 package arrays;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 /*
  * Given an array of strings, group the anagrams together.
@@ -13,20 +14,44 @@ public class GroupAnagramsNoSort {
         String[] strings1 = {"eat", "tea", "tan", "ate", "nat", "bat"};
         String[] strings2 = {""};
         String[] strings3 = {"a"};
+        String[] strings4 = {"bdddddddddd","bbbbbbbbbbc"};
 
         System.out.println(groupAnagrams(strings1));
         System.out.println(groupAnagrams(strings2));
         System.out.println(groupAnagrams(strings3));
+        System.out.println(groupAnagrams(strings4));
 
     }
 
     public static List<List<String>> groupAnagrams(String[] strs) {
 
-        Map<String, List<String>> stringSortedChars = new HashMap<>();
+        Map<String, List<String>> result = new HashMap<>();
 
+        for (String str : strs) {
 
-        return stringSortedChars.values().stream()
-                .toList();
+            String charCount = createCharCountString(str);
+
+            if (!result.containsKey(charCount)) {
+                result.put(charCount, new ArrayList<>());
+            }
+            result.get(charCount).add(str);
+        }
+
+        return result.values().stream().toList();
+    }
+
+    private static String createCharCountString(String str) {
+
+        int[] result = new int[26];
+
+        for (char character : str.toCharArray()) {
+            int charIndex = ((int) character) - ((int) 'a');
+            result[charIndex] = result[charIndex] + 1;
+        }
+
+        return Arrays.stream(result).mapToObj(String::valueOf)
+                .collect(Collectors.joining("_"));
+
     }
 
 }
